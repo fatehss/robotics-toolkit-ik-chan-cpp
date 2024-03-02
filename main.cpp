6777e4d092004ef42d0100085164ecb4e8a32647
fatehss <fatehss@g.ucla.edu>
2024-03-02 11:04:22 -0330

    remove c ifndef stuff
    I have removed all of the lines like extern C{...} and #ifdef __cplusplus. hopefully it doesn't fuck everything up

--- main.cpp
+++ main.cpp
@@ -6,121 +6,121 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <math.h>
-    
-    void _IK_LM_Chan(
-        ETS *ets, Matrix4dc Tep,
-        MapVectorX q0, int ilimit, int slimit, double tol, int reject_jl,
-        MapVectorX q, int *it, int *search, int *solution, double *E,
-        double lambda, MapVectorX we)
+
+void _IK_LM_Chan(
+    ETS *ets, Matrix4dc Tep,
+    MapVectorX q0, int ilimit, int slimit, double tol, int reject_jl,
+    MapVectorX q, int *it, int *search, int *solution, double *E,
+    double lambda, MapVectorX we)
+{
+    int iter = 1;
+
+    // std::cout << Tep << "\n";
+    // std::cout << std::endl;
+
+    double *np_Te = new double[16];
+    MapMatrix4dc Te(np_Te);
+
+    double *np_J = new double[6 * ets->n];
+    Eigen::Map<Eigen::MatrixXd> J(np_J, 6, ets->n);
+
+    double *np_e = new double[6];
+    MapVectorX e(np_e, 6);
+
+    Matrix6dc We;
+
+    Eigen::MatrixXd Wn(ets->n, ets->n);
+    Eigen::MatrixXd EyeN = Eigen::MatrixXd::Identity(ets->n, ets->n);
+
+    VectorX g(ets->n);
+
+    // Set we
+    if (we.size() == 6)
     {
-        int iter = 1;
+        We = we.asDiagonal();
+    }
+    else
+    {
+        We = Matrix6dc::Identity();
+    }
 
-        // std::cout << Tep << "\n";
-        // std::cout << std::endl;
+    // Set the first q0
+    if (q0.size() == ets->n)
+    {
+        q = q0;
+    }
+    else
+    {
+        _rand_q(ets, q);
+    }
 
-        double *np_Te = new double[16];
-        MapMatrix4dc Te(np_Te);
+    // Global search up to slimit
+    while (*search <= slimit)
+    {
 
-        double *np_J = new double[6 * ets->n];
-        Eigen::Map<Eigen::MatrixXd> J(np_J, 6, ets->n);
-
-        double *np_e = new double[6];
-        MapVectorX e(np_e, 6);
-
-        Matrix6dc We;
-
-        Eigen::MatrixXd Wn(ets->n, ets->n);
-        Eigen::MatrixXd EyeN = Eigen::MatrixXd::Identity(ets->n, ets->n);
-
-        VectorX g(ets->n);
-
-        // Set we
-        if (we.size() == 6)
+        while (iter <= ilimit)
         {
-            We = we.asDiagonal();
-        }
-        else
-        {
-            We = Matrix6dc::Identity();
-        }
+            // Current pose Te
+            _ETS_fkine(ets, q.data(), (double *)NULL, NULL, Te);
 
-        // Set the first q0
-        if (q0.size() == ets->n)
-        {
-            q = q0;
-        }
-        else
-        {
-            _rand_q(ets, q);
-        }
+            // Angle axis error e
+            _angle_axis(Te, Tep, e);
 
-        // Global search up to slimit
-        while (*search <= slimit)
-        {
+            // Squared error E
+            *E = 0.5 * e.transpose() * We * e;
 
-            while (iter <= ilimit)
+            if (*E < tol)
             {
-                // Current pose Te
-                _ETS_fkine(ets, q.data(), (double *)NULL, NULL, Te);
+                // We have arrived
 
-                // Angle axis error e
-                _angle_axis(Te, Tep, e);
-
-                // Squared error E
-                *E = 0.5 * e.transpose() * We * e;
-
-                if (*E < tol)
+                // wrap q to +- pi
+                for (int i = 0; i < ets->n; i++)
                 {
-                    // We have arrived
-
-                    // wrap q to +- pi
-                    for (int i = 0; i < ets->n; i++)
-                    {
-                        q(i) = std::fmod(q(i) + PI, PI_x2) - PI;
-                    }
-
-                    // Check for joint limit violation
-                    if (reject_jl)
-                    {
-                        *solution = _check_lim(ets, q);
-                    }
-                    else
-                    {
-                        *solution = 1;
-                    }
-
-                    break;
+                    q(i) = std::fmod(q(i) + PI, PI_x2) - PI;
                 }
 
-                // Jacobian Matric J
-                _ETS_jacob0(ets, q.data(), (double *)NULL, J);
+                // Check for joint limit violation
+                if (reject_jl)
+                {
+                    *solution = _check_lim(ets, q);
+                }
+                else
+                {
+                    *solution = 1;
+                }
 
-                // Weighting matrix Wn
-                Wn = lambda * *E * EyeN;
-
-                // The vector g
-                g = J.transpose() * We * e;
-
-                // Work out the joint velocity qd
-                q += (J.transpose() * We * J + Wn).inverse() * g;
-                // q += (J.transpose() * We * J + Wn).colPivHouseholderQr().solve(g);
-
-                iter += 1;
-            }
-
-            if (*solution)
-            {
-                *it += iter;
                 break;
             }
 
-            *it += iter;
-            iter = 0;
-            *search += 1;
-            _rand_q(ets, q);
+            // Jacobian Matric J
+            _ETS_jacob0(ets, q.data(), (double *)NULL, J);
+
+            // Weighting matrix Wn
+            Wn = lambda * *E * EyeN;
+
+            // The vector g
+            g = J.transpose() * We * e;
+
+            // Work out the joint velocity qd
+            q += (J.transpose() * We * J + Wn).inverse() * g;
+            // q += (J.transpose() * We * J + Wn).colPivHouseholderQr().solve(g);
+
+            iter += 1;
         }
 
-        delete[] np_Te;
-        delete[] np_J;
-        delete[] np_e; 
+        if (*solution)
+        {
+            *it += iter;
+            break;
+        }
+
+        *it += iter;
+        iter = 0;
+        *search += 1;
+        _rand_q(ets, q);
     }
+
+    delete[] np_Te;
+    delete[] np_J;
+    delete[] np_e; 
+}
--- structs.h
+++ structs.h
@@ -14,49 +14,40 @@
 
 #include "linalg.h"
 
-#ifdef __cplusplus
-extern "C"
+struct ETS
 {
-#endif /* __cplusplus */
+        /**********************************************************
+         *************** kinematic parameters *********************
+                **********************************************************/
+        ET **ets;
+        int n;
+        int m;
 
-        typedef struct ET ET;
-        typedef struct ETS ETS;
+        // While this information is stored in the ET's
+        // Its much faster for IK to cache it here
+        double *qlim_l;
+        double *qlim_h;
+        double *q_range2;
+};
 
-        struct ETS
-        {
-                /**********************************************************
-                 *************** kinematic parameters *********************
-                 **********************************************************/
-                ET **ets;
-                int n;
-                int m;
+struct ET
+{
+        int isstaticsym; /* this ET is static and has a symbolic value */
+        int isjoint;
+        int isflip;
+        int jindex;
+        int axis;
+        double *T;    /* link static transform */
+        double *qlim; /* joint limits */
+        void (*op)(double *data, double eta);
 
-                // While this information is stored in the ET's
-                // Its much faster for IK to cache it here
-                double *qlim_l;
-                double *qlim_h;
-                double *q_range2;
-        };
+        // #ifdef __cplusplus
+        // Eigen::Map<Eigen::Matrix<double, 4, 4, Eigen::RowMajor>> Tm;
+        MapMatrix4dc Tm;
+        // #endif /* __cplusplus */
 
-        struct ET
-        {
-                int isstaticsym; /* this ET is static and has a symbolic value */
-                int isjoint;
-                int isflip;
-                int jindex;
-                int axis;
-                double *T;    /* link static transform */
-                double *qlim; /* joint limits */
-                void (*op)(double *data, double eta);
+};
 
-                // #ifdef __cplusplus
-                // Eigen::Map<Eigen::Matrix<double, 4, 4, Eigen::RowMajor>> Tm;
-                MapMatrix4dc Tm;
-                // #endif /* __cplusplus */
-        };
 
-#ifdef __cplusplus
-} /* extern "C" */
-#endif /* __cplusplus */
 
 #endif
